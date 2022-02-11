f1d150c5dbf06059c1556e11f2c13d29a8739acc
fastmed <maks.memelige@gmail.com>
2022-02-11 12:01:12 +0200

    Add Task model

--- api/models.py
+++ api/models.py
@@ -7,3 +7,33 @@ class User(models.Model):
     level = models.IntegerField(verbose_name="Уровень пользователя")
     coins = models.IntegerField(verbose_name="Количество монет у пользователя")
     picture_id = models.IntegerField(verbose_name="ID картинки")
+
+
+class Task(models.Model):
+    IMPORTANCE = [
+        ("OPT", "Optional"),
+        ("REQ", "Required"),
+        ("URG", "Urgently"),
+    ]
+
+    FREQUENCY = [
+        ("ONCE","Once"),
+        ("DAY","Once_a_day"),
+        ("WEEK","Once_a_week"),
+    ]
+
+    COMPLEXITY = [
+        ("EASY","Easy"),
+        ("MEDIUM","Medium"),
+        ("HARD","Hard"),
+    ]
+
+    task_id = models.CharField(verbose_name="ID Задачи", max_length="60")
+    name = models.CharField(verbose_name="Название задачи", max_length="60")
+    picture = models.ImageField(verbose_name="Картинка задачи")
+    description = models.TextField(verbose_name="Описание задачи")
+    expired_at = models.DateField(verbose_name="Дата окончания задачи")
+    importance = models.CharField(verbose_name="Важность задачи", max_length="60", choices=IMPORTANCE)
+    frequency = models.CharField(verbose_name="Частота задачи", max_length="60", choices=FREQUENCY)
+    complexity = models.CharField(verbose_name="Сложность", max_length="60", choices=COMPLEXITY)
+    is_complete = models.BooleanField(verbose_name="Выполнена ли задача")
