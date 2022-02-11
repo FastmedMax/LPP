5f7aee59bc4868056b5d989dfd27ecdd8faedc33
fastmed <maks.memelige@gmail.com>
2022-02-11 14:30:26 +0200

    Fix incorrect type of max_length

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id_vk = models.CharField(verbose_name="ID пользователя в VK", max_length="60")
+    user_id = models.CharField(verbose_name="ID пользователя в VK", max_length=60)
     experience = models.IntegerField(verbose_name="Опыт пользователя")
     level = models.IntegerField(verbose_name="Уровень пользователя")
     coins = models.IntegerField(verbose_name="Количество монет у пользователя")
@@ -28,14 +28,14 @@ class Task(models.Model):
         ("HARD","Hard"),
     ]
 
-    task_id = models.CharField(verbose_name="ID Задачи", max_length="60")
-    name = models.CharField(verbose_name="Название задачи", max_length="60")
+    task_id = models.CharField(verbose_name="ID Задачи", max_length=60)
+    name = models.CharField(verbose_name="Название задачи", max_length=60)
     picture = models.ImageField(verbose_name="Картинка задачи")
     description = models.TextField(verbose_name="Описание задачи")
     expired_at = models.DateField(verbose_name="Дата окончания задачи")
-    importance = models.CharField(verbose_name="Важность задачи", max_length="60", choices=IMPORTANCE)
-    frequency = models.CharField(verbose_name="Частота задачи", max_length="60", choices=FREQUENCY)
-    complexity = models.CharField(verbose_name="Сложность", max_length="60", choices=COMPLEXITY)
+    importance = models.CharField(verbose_name="Важность задачи", max_length=60, choices=IMPORTANCE)
+    frequency = models.CharField(verbose_name="Частота задачи", max_length=60, choices=FREQUENCY)
+    complexity = models.CharField(verbose_name="Сложность", max_length=60, choices=COMPLEXITY)
     is_complete = models.BooleanField(verbose_name="Выполнена ли задача")
 
 
