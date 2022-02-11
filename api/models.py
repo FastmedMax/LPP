b6fad937e34c825308038aa8715a088a819f3924
fastmed <maks.memelige@gmail.com>
2022-02-11 12:00:49 +0200

    Add User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id_vk = models.CharField(verbose_name="ID пользователя в VK", max_length="60")
+    experience = models.IntegerField(verbose_name="Опыт пользователя")
+    level = models.IntegerField(verbose_name="Уровень пользователя")
+    coins = models.IntegerField(verbose_name="Количество монет у пользователя")
+    picture_id = models.IntegerField(verbose_name="ID картинки")
