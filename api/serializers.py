eed0d9e3e28114644d50ea1373634e87a76d356d
fastmed <maks.memelige@gmail.com>
2022-02-13 16:00:55 +0200

    Add AwardSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from .models import User, Award, Task, Goods, UserAward
+
+
+class AwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = "__all__"
+
+
