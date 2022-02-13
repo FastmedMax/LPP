d9fe3c8ef0cee601a776d318bc0a77f12e64c794
fastmed <maks.memelige@gmail.com>
2022-02-13 19:18:22 +0200

    Add api url for award list

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import UserView, TaskListView, TaskListWeekView, GoodsView, TaskView
+from .views import UserView, TaskListView, TaskListWeekView, GoodsView, TaskView, AwardListView
 
 
 urlpatterns = [
     path("users/", UserView.as_view()),
     path("users/<str:user_id>/", UserView.as_view()),
+    path("awards/", AwardListView.as_view()),
     path("tasks/", TaskView.as_view()),
     path("tasks/<str:user_id>/", TaskListView.as_view()),
     path("tasks/week/<str:user_id>/", TaskListWeekView.as_view()),
