67784ced8d65f02809fd15a9725cd468dd7556f3
fastmed <maks.memelige@gmail.com>
2022-02-13 16:54:08 +0200

    Register models in admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from .models import User, Task, Award, Goods
+
 # Register your models here.
+admin.site.register(Award)
+admin.site.register(Goods)
+
+
+class UserAwardlist(admin.StackedInline):
+    model = User.awards.through
+    extra = 0
+    classes = ["collapse"]
+
+
+class UserGoodsList(admin.StackedInline):
+    model = User.goods.through
+    extra = 0
+    classes = ["collapse"]
+
+
+class UserTasksList(admin.StackedInline):
+    model = Task
+    extra = 0
+    classes = ["collapse"]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (UserAwardlist, UserGoodsList, UserTasksList)
+    exclude = ("goods",)
