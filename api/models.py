ca1c8204f4a5bc00315d23cf0ff84d27166532ca
fastmed <maks.memelige@gmail.com>
2022-02-13 15:43:06 +0200

    Add imports

--- api/models.py
+++ api/models.py
@@ -1,4 +1,8 @@
+import enum
+
+from django.utils import timezone
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class User(models.Model):
