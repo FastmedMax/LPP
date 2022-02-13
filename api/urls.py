d00c031f80d80e22b967a26b4b606821bd8b94e3
fastmed <maks.memelige@gmail.com>
2022-02-13 21:34:56 +0200

    Add new api view and change place other

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path("users/<str:user_id>/", UserView.as_view()),
     path("awards/", AwardListView.as_view()),
     path("tasks/", TaskView.as_view()),
-    path("tasks/<str:user_id>/", TaskListView.as_view()),
+    path("tasks/<str:task_id>/", TaskView.as_view()),
     path("tasks/week/<str:user_id>/", TaskListWeekView.as_view()),
+    path("tasks/<str:user_id>/", TaskListView.as_view()),
     path("tasks/<str:user_id>/<str:day>/", TaskListView.as_view()),
     path("goods/buy/", GoodsView.as_view())
 ]
